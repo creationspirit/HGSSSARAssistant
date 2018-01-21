4ffe1ccb01b7754db101b22089dd929389ada5e3
Matej Janjić <matej.janjic@infinum.hr>
2018-01-21 18:25:38 +0100

    Remove templated views

--- HGSSSARAssistant.Web/Controllers/HomeController.cs
+++ HGSSSARAssistant.Web/Controllers/HomeController.cs
@@ -12,23 +12,9 @@ namespace HGSSSARAssistant.Web.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return Redirect("Users");
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
