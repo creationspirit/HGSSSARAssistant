a09af2712684b5f1f77d387aadb96e35ba288247
Matej Janjić <matej.janjic@infinum.hr>
2018-01-23 15:32:35 +0100

    Reroute home controller

--- HGSSSARAssistant.Web/Controllers/HomeController.cs
+++ HGSSSARAssistant.Web/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace HGSSSARAssistant.Web.Controllers
     {
         public IActionResult Index()
         {
-            return Redirect("Users");
+            return View();
         }
         
         public IActionResult Error()
