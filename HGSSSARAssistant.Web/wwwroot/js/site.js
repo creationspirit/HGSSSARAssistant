8493de991379457179e82b4ba70d4073272bc981
Luka Lazanja <luka.lazanja@fer.hr>
2018-01-24 13:17:29 +0100

    Added moment, fullcalendar

--- HGSSSARAssistant.Web/wwwroot/js/site.js
+++ HGSSSARAssistant.Web/wwwroot/js/site.js
@@ -1,1 +1,27 @@
-﻿// Write your JavaScript code.
+﻿$(document).ready(function () {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+        header: {
+            left: 'Calendar',
+            center: '',
+            right: ''
+        },
+        views: {
+            dayOfWeekView: {
+                type: 'agendaWeek',
+                duration: {
+                    days: 7
+                },
+                columnFormat: 'dddd', // Format the day to only show like 'Monday'
+                slotLabelFormat: 'h(:mm)a',
+                allDaySlot: false,
+                slotDuration: '00:30:00'
+            }
+        },
+        defaultView: 'dayOfWeekView',
+    })
+
+});
