6abe8b089afd7fed1dd6a29cb59a6de4c9b38c46
Matej Janjic <matej.janjic@infinum.hr>
2018-01-25 18:27:50 +0100

    Add example failing test

---
+++ HGSSSARAssistant.Test/Core/LocationTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HGSSSARAssistant.Test.Core
+{
+    public class LocationTest
+    {
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(1, 3);
+        }
+    }
+}
